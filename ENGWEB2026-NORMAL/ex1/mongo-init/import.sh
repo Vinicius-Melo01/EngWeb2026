8ccaf81c783fe3774e32c4265fdcfcd162fefa20
Vinicius Melo <73396807+Vinicius-Melo01@users.noreply.github.com>
2026-05-25 11:52:35 +0100

    Delete ENGWEB2026-NORMAL directory

--- ENGWEB2026-NORMAL/ex1/mongo-init/import.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mongoimport --db jogostabuleiro --collection jogos --jsonArray --file /docker-entrypoint-initdb.d/jogos_import.json
--- ENGWEB2026-NORMAL/ex2/mongo-init/import.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mongoimport --db leituras --collection livros --jsonArray --file /docker-entrypoint-initdb.d/dataset.json
